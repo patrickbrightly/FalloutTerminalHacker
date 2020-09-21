f4e3fd5d8c6be9fa80adbe276f973d0fe6be3bb6
patrickbrightly <patrickfbright@gmail.com>
2020-09-21 16:15:47 -0400

    Updates

---
+++ precache-manifest.a3884b3ce091abca445fd4a85c0b1abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8a9b9ad1d234135d0a148a2c141315",
+    "url": "/patrickbrightly/FalloutTerminalHacker/index.html"
+  },
+  {
+    "revision": "052832694ee38a368094",
+    "url": "/patrickbrightly/FalloutTerminalHacker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "052832694ee38a368094",
+    "url": "/patrickbrightly/FalloutTerminalHacker/static/js/2.1e75347e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/patrickbrightly/FalloutTerminalHacker/static/js/2.1e75347e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba7a4fcd533dfc797a1",
+    "url": "/patrickbrightly/FalloutTerminalHacker/static/js/main.b8f6539a.chunk.js"
+  },
+  {
+    "revision": "dfec0d84d684dfa97438",
+    "url": "/patrickbrightly/FalloutTerminalHacker/static/js/runtime-main.82d73274.js"
+  }
+]);
